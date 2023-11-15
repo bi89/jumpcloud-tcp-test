a04588c563c8adb82aa8c19e6dba71efa01a4576
Suchathit Boonnag <bi1989@gmail.com>
2023-11-15 18:51:43 +0800

    Update test-tcp.ps1

--- test-tcp.ps1
+++ test-tcp.ps1
@@ -1,9 +1,9 @@
 #TCP check for JumpCloud white list
 #version 0.1
 #Boon boon.suchathit@techdirect.net
-#8/11/23
+#15/11/23
 #Note: pmw.jumpcloud.com is using private DNS record.
-$domain = @("a1hrq03pdcca60-ats.iot.us-east-1.amazonaws.com","agent.jumpcloud.com","assist.jumpcloud.com","cdn02.jumpcloud.com","chocolatey.org","kickstart.jumpcloud.com","private-kickstart.jumpcloud.com","s3.amazonaws.com")
+$domain = @("a1hrq03pdcca60-ats.iot.us-east-1.amazonaws.com","agent.jumpcloud.com","assist.jumpcloud.com","cdn02.jumpcloud.com","chocolatey.org","kickstart.jumpcloud.com","private-kickstart.jumpcloud.com","s3.amazonaws.com","cdn.pwm.jumpcloud.com")
 for ($i=0;$i -lt$domain.Length;$i++){
     $test = Test-Netconnection -Port 443 $domain[$i] | Select-Object -Property TcpTestSucceeded -ExpandProperty TcpTestSucceeded
     if ($test -eq $true){
